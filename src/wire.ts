691e6e9d5789236b1a7659af6629be09551950d6
stasbar <stasbar1995@gmail.com>
2019-08-03 11:14:36 +0200

    add isComplex to wire type

--- src/wire.ts
+++ src/wire.ts
@@ -1,111 +1,113 @@
-import { KANTHAL_A1_AMP, Material } from './material';
+import {KANTHAL_A1_AMP, Material} from './material';
 
 export enum WireType {
-  CUSTOM = -1,
-  NORMAL,
-  PARALLEL,
-  TWISTED,
-  CLAPTON,
-  RIBBON,
-  FUSED_CLAPTON,
-  ALIEN_CLAPTON,
-  TIGER,
-  STAPLE,
-  STAGGERED_CLAPTON,
-  STAGGERED_FUSED_CLAPTON,
-  STAPLE_STAGGERED_FUSED_CLAPTON,
-  FRAMED_STAPLE,
-  JUGGERNAUT,
+    CUSTOM = -1,
+    NORMAL,
+    PARALLEL,
+    TWISTED,
+    CLAPTON,
+    RIBBON,
+    FUSED_CLAPTON,
+    ALIEN_CLAPTON,
+    TIGER,
+    STAPLE,
+    STAGGERED_CLAPTON,
+    STAGGERED_FUSED_CLAPTON,
+    STAPLE_STAGGERED_FUSED_CLAPTON,
+    FRAMED_STAPLE,
+    JUGGERNAUT,
 }
 
 export enum WireFormat {
-  NORMAL,
-  STAGGERED
+    NORMAL,
+    STAGGERED
 }
 
 export enum WireStyle {
-  CORE,
-  OUTER
+    CORE,
+    OUTER
 }
 
 export enum WireKind {
-  ROUND,
-  RIBBON
+    ROUND,
+    RIBBON
 }
 
 export interface WiresTree {
-  type: WireType;
-  cores: Wire[];
-  outers: Wire[];
-  pitch: number;
+    type: WireType;
+    cores: Wire[];
+    outers: Wire[];
+    pitch: number;
 }
 
 export class Wire implements WiresTree {
-  type: WireType;
+    type: WireType;
 
-  material: Material;
+    material: Material;
 
-  style: WireStyle;
+    style: WireStyle;
 
-  format: WireFormat;
+    format: WireFormat;
 
-  kind: WireKind;
+    kind: WireKind;
 
-  mm: number;
+    mm: number;
 
-  width: number;
+    width: number;
 
-  height: number;
+    height: number;
 
-  pitch: number;
+    pitch: number;
 
-  space: number;
+    space: number;
 
-  innerDiameter: number;
+    innerDiameter: number;
 
-  totalLength: number;
+    totalLength: number;
 
-  wrapLength: number;
+    wrapLength: number;
 
-  widthDiameter: number;
+    widthDiameter: number;
 
-  heightDiameter: number;
+    heightDiameter: number;
 
-  resistance: number;
+    resistance: number;
 
-  cores: Wire[];
+    cores: Wire[];
 
-  outers: Wire[];
+    outers: Wire[];
 
-  constructor({
-                type = WireType.NORMAL, material = KANTHAL_A1_AMP, style = WireStyle.CORE,
-                format = WireFormat.NORMAL, kind = WireKind.ROUND, mm = 0.0, width = 0.0,
-                height = 0.0, pitch = 0.0, space = 0.0, innerDiameter = 0.0, totalLength = 0.0,
-                wrapLength = 0.0, widthDiameter = 0.0, heightDiameter = 0.0, resistance = 0.0,
-                cores = [], outers = [],
-              }: {
-    type?: WireType, material?: Material, style?: WireStyle, format?: WireFormat,
-    kind?: WireKind, mm?: number, width?: number, height?: number, pitch?: number, space?: number,
-    innerDiameter?: number, totalLength?: number, wrapLength?: number, widthDiameter?: number,
-    heightDiameter?: number, resistance?: number, cores?: Wire[], outers?: Wire[]
-  } = {}) {
-    this.type = type;
-    this.material = material;
-    this.style = style;
-    this.format = format;
-    this.kind = kind;
-    this.mm = mm;
-    this.width = width;
-    this.height = height;
-    this.pitch = pitch;
-    this.space = space;
-    this.innerDiameter = innerDiameter;
-    this.totalLength = totalLength;
-    this.wrapLength = wrapLength;
-    this.widthDiameter = widthDiameter;
-    this.heightDiameter = heightDiameter;
-    this.resistance = resistance;
-    this.cores = cores;
-    this.outers = outers;
-  }
+    isComplex = () => this.cores.length > 0 || this.outers.length > 0;
+
+    constructor({
+                    type = WireType.NORMAL, material = KANTHAL_A1_AMP, style = WireStyle.CORE,
+                    format = WireFormat.NORMAL, kind = WireKind.ROUND, mm = 0.0, width = 0.0,
+                    height = 0.0, pitch = 0.0, space = 0.0, innerDiameter = 0.0, totalLength = 0.0,
+                    wrapLength = 0.0, widthDiameter = 0.0, heightDiameter = 0.0, resistance = 0.0,
+                    cores = [], outers = [],
+                }: {
+        type?: WireType, material?: Material, style?: WireStyle, format?: WireFormat,
+        kind?: WireKind, mm?: number, width?: number, height?: number, pitch?: number, space?: number,
+        innerDiameter?: number, totalLength?: number, wrapLength?: number, widthDiameter?: number,
+        heightDiameter?: number, resistance?: number, cores?: Wire[], outers?: Wire[]
+    } = {}) {
+        this.type = type;
+        this.material = material;
+        this.style = style;
+        this.format = format;
+        this.kind = kind;
+        this.mm = mm;
+        this.width = width;
+        this.height = height;
+        this.pitch = pitch;
+        this.space = space;
+        this.innerDiameter = innerDiameter;
+        this.totalLength = totalLength;
+        this.wrapLength = wrapLength;
+        this.widthDiameter = widthDiameter;
+        this.heightDiameter = heightDiameter;
+        this.resistance = resistance;
+        this.cores = cores;
+        this.outers = outers;
+    }
 }
