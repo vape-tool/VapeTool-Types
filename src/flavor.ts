a860cb6fdc65d4eca21a53807a8042609c5a1c98
stasbar <stasbar1995@gmail.com>
2019-08-11 23:45:57 +0200

    fix types add constructors
    Took 13 minutes

---
+++ src/flavor.ts
@@ -0,0 +1,39 @@
+import uuid from './uuid';
+import {Author, LOCAL_AUTHOR, OnlineStatus, Storeable} from "./index";
+
+export default class Flavor implements Storeable {
+    uid: string;
+    author: Author;
+    creationTime: number;
+    lastTimeModified: number;
+    status: OnlineStatus;
+    name: string;
+    manufacturer: string;
+    percentage: number;
+    ratio: number; //PG ratio
+    price: number;
+    amount: number;
+
+    constructor(
+        {
+            uid = uuid(), author = LOCAL_AUTHOR, creationTime = Date.now(), lastTimeModified = Date.now(),
+            status = OnlineStatus.ONLINE_PRIVATE, name = '', manufacturer = '', percentage = 0, ratio = 0,
+            price = 0, amount = 0
+        }: {
+            uid?: string, author?: Author, creationTime?: number, lastTimeModified?: number,
+            status?: OnlineStatus, name?: string, manufacturer?: string, percentage?: number, ratio?: number,
+            price?: number, amount?: number
+        } = {}) {
+        this.uid = uid;
+        this.author = author;
+        this.creationTime = creationTime;
+        this.lastTimeModified = lastTimeModified;
+        this.status = status;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.percentage = percentage;
+        this.ratio = ratio;
+        this.price = price;
+        this.amount = amount;
+    }
+}
