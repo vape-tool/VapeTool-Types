aaecb930456dbbf1c6b78671e0db7fc492354efe
stasbar <stasbar1995@gmail.com>
2019-08-21 18:21:23 +0200

    update battery id function
    Took 43 seconds

--- src/battery.ts
+++ src/battery.ts
@@ -24,5 +24,5 @@ export class Battery {
 }
 
 export function id(battery: Battery): string {
-    return `${battery.brand}-${battery.model}-${battery.size}-${battery.capacity}-${battery.stableCurrent}`
+    return `${battery.brand}-${battery.model}-${battery.size}-${battery.capacity}`
 }
