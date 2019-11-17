83205e6a6e8e8f10633222fcb3892414b11e31c0
Stanislaw Baranski <stachu@stasbar.com>
2019-11-17 21:05:26 +0100

    add Coil Properties class

--- src/coil.ts
+++ src/coil.ts
@@ -1,60 +1,118 @@
-import uuid from './uuid';
-import {Author, LOCAL_AUTHOR, OnlineStatus, Storeable, Wire, WireType} from './index';
+import uuid from "./uuid";
+import {
+  Author,
+  LOCAL_AUTHOR,
+  OnlineStatus,
+  Storeable,
+  Wire,
+  WireType
+} from "./index";
 
-export default class Coil implements Storeable {
-    uid: string;
-    name: string;
-    description: string;
-    author: Author;
-    creationTime: number;
-    lastTimeModified: number;
-    status: OnlineStatus;
-    type: WireType;
-    setup: number;
-    wraps: number;
-    resistance: number;
-    legsLength: number;
-    innerDiameter: number;
-    pitch: number;
-    heightDiameter: number;
-    widthDiameter: number;
-    totalLength: number;
-    cores: Wire[];
-    outers: Wire[];
+export class Coil implements Storeable {
+  uid: string;
+  name: string;
+  description: string;
+  author: Author;
+  creationTime: number;
+  lastTimeModified: number;
+  status: OnlineStatus;
+  type: WireType;
+  setup: number;
+  wraps: number;
+  resistance: number;
+  legsLength: number;
+  innerDiameter: number;
+  pitch: number;
+  heightDiameter: number;
+  widthDiameter: number;
+  totalLength: number;
+  cores: Wire[];
+  outers: Wire[];
 
-    constructor({
-                    uid = uuid(), name = '', description = '', author = LOCAL_AUTHOR,
-                    creationTime = Date.now(), lastTimeModified = Date.now(),
-                    status = OnlineStatus.ONLINE_PRIVATE, type = WireType.NORMAL,
-                    setup = 1, wraps = 5.0, resistance = 0.554,
-                    legsLength = 15.0, innerDiameter = 3.0, pitch = 0.0,
-                    heightDiameter = 0.0, widthDiameter = 0.0, totalLength = 0.0,
-                    cores = [], outers = [],
-                }: {
-        uid?: string, name?: string, description?: string, author?: Author, creationTime?: number,
-        lastTimeModified?: number, status?: OnlineStatus, type?: WireType, setup?: number,
-        wraps?: number, resistance?: number, legsLength?: number, innerDiameter?: number,
-        pitch?: number, heightDiameter?: number, widthDiameter?: number, totalLength?: number,
-        cores?: Wire[], outers?: Wire[]
-    } = {}) {
-        this.setup = setup;
-        this.name = name;
-        this.description = description;
-        this.wraps = wraps;
-        this.resistance = resistance;
-        this.legsLength = legsLength;
-        this.type = type;
-        this.author = author;
-        this.creationTime = creationTime;
-        this.lastTimeModified = lastTimeModified;
-        this.status = status;
-        this.uid = uid;
-        this.cores = cores;
-        this.outers = outers;
-        this.innerDiameter = innerDiameter;
-        this.pitch = pitch;
-        this.heightDiameter = heightDiameter;
-        this.widthDiameter = widthDiameter;
-        this.totalLength = totalLength;
-    }
+  constructor({
+    uid = uuid(),
+    name = "",
+    description = "",
+    author = LOCAL_AUTHOR,
+    creationTime = Date.now(),
+    lastTimeModified = Date.now(),
+    status = OnlineStatus.ONLINE_PRIVATE,
+    type = WireType.NORMAL,
+    setup = 1,
+    wraps = 5.0,
+    resistance = 0.554,
+    legsLength = 15.0,
+    innerDiameter = 3.0,
+    pitch = 0.0,
+    heightDiameter = 0.0,
+    widthDiameter = 0.0,
+    totalLength = 0.0,
+    cores = [],
+    outers = []
+  }: {
+    uid?: string;
+    name?: string;
+    description?: string;
+    author?: Author;
+    creationTime?: number;
+    lastTimeModified?: number;
+    status?: OnlineStatus;
+    type?: WireType;
+    setup?: number;
+    wraps?: number;
+    resistance?: number;
+    legsLength?: number;
+    innerDiameter?: number;
+    pitch?: number;
+    heightDiameter?: number;
+    widthDiameter?: number;
+    totalLength?: number;
+    cores?: Wire[];
+    outers?: Wire[];
+  } = {}) {
+    this.setup = setup;
+    this.name = name;
+    this.description = description;
+    this.wraps = wraps;
+    this.resistance = resistance;
+    this.legsLength = legsLength;
+    this.type = type;
+    this.author = author;
+    this.creationTime = creationTime;
+    this.lastTimeModified = lastTimeModified;
+    this.status = status;
+    this.uid = uid;
+    this.cores = cores;
+    this.outers = outers;
+    this.innerDiameter = innerDiameter;
+    this.pitch = pitch;
+    this.heightDiameter = heightDiameter;
+    this.widthDiameter = widthDiameter;
+    this.totalLength = totalLength;
+  }
+}
+
+export class Properties {
+  current: number;
+  power: number;
+  heat: number;
+  surface: number;
+  sweetSpotLow: number;
+  sweetSpotHigh: number;
+
+  constructor(
+    current: number,
+    power: number,
+    heat: number,
+    surface: number,
+    sweetSpotLow: number,
+    sweetSpotHigh: number
+  ) {
+    this.current = current;
+    this.power = power;
+    this.heat = heat;
+    this.surface = surface;
+    this.sweetSpotLow = sweetSpotLow;
+    this.sweetSpotHigh = sweetSpotHigh;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,55 @@
-import Coil from './coil'
-import {Wire, WireStyle, WireFormat, WireType, WireKind, isComplex} from './wire'
-import {Material} from './material'
-import * as Materials from './material'
-import {Author, LOCAL_AUTHOR, OnlineStatus, Storeable, isCloudyValid} from './cloud'
-import * as wireGenerator from './wireGenerator'
-import {User, Ban, UserPermission} from './user';
-import {OnlineContentStatus, Photo} from './photo';
-import Flavor from './flavor';
-import {Liquid, Result} from './liquid';
-import {Battery, id} from "./battery";
-import Comment from './comment';
+import { Coil, Properties } from "./coil";
+import {
+  Wire,
+  WireStyle,
+  WireFormat,
+  WireType,
+  WireKind,
+  isComplex
+} from "./wire";
+import { Material } from "./material";
+import * as Materials from "./material";
+import {
+  Author,
+  LOCAL_AUTHOR,
+  OnlineStatus,
+  Storeable,
+  isCloudyValid
+} from "./cloud";
+import * as wireGenerator from "./wireGenerator";
+import { User, Ban, UserPermission } from "./user";
+import { OnlineContentStatus, Photo } from "./photo";
+import Flavor from "./flavor";
+import { Liquid, Result } from "./liquid";
+import { Battery, id } from "./battery";
+import Comment from "./comment";
 
 export {
-    wireGenerator,
-    Coil,
-    Wire,
-    WireStyle,
-    WireFormat,
-    WireType,
-    WireKind,
-    Material,
-    Materials,
-    Author,
-    LOCAL_AUTHOR,
-    OnlineStatus,
-    Storeable,
-    isCloudyValid,
-    isComplex,
-    User,
-    Ban,
-    UserPermission,
-    Photo,
-    OnlineContentStatus,
-    Flavor,
-    Liquid,
-    Result,
-    Battery,
-    id,
-    Comment
-}
+  wireGenerator,
+  Coil,
+  Properties,
+  Wire,
+  WireStyle,
+  WireFormat,
+  WireType,
+  WireKind,
+  Material,
+  Materials,
+  Author,
+  LOCAL_AUTHOR,
+  OnlineStatus,
+  Storeable,
+  isCloudyValid,
+  isComplex,
+  User,
+  Ban,
+  UserPermission,
+  Photo,
+  OnlineContentStatus,
+  Flavor,
+  Liquid,
+  Result,
+  Battery,
+  id,
+  Comment
+};
