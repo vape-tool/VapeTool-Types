6a8ce021ffc4bbb729602c2937f8ed2aeab982bb
stasbar <stasbar1995@gmail.com>
2020-05-29 18:13:14 +0200

    remove EnumType names

--- src/mixable.ts
+++ src/mixable.ts
@@ -1,7 +1,7 @@
 import { Result } from "./liquid";
 
 export enum MixableType {
-    BASE = 'base', PREMIX = 'premix', LIQUID = 'liquid'
+    BASE, PREMIX, LIQUID
 }
 export default class Mixable {
     constructor(readonly type: MixableType, readonly amount: number, readonly strength: number, readonly ratio: number, readonly thinner: number) {
